a60c2dce28ef78902ca6f427ce7e696342e9b897
saphaltaneupane <neupanesaphalta@gmail.com>
2023-12-31 17:31:28 +0545

    ok

--- jframe_sample.java
+++ jframe_sample.java
@@ -5,11 +5,18 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import java.awt.Font;
+import java.awt.Color;
 
 public class jframe_sample extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField txtWelcomeToSupermarket;
 
 	/**
 	 * Launch the application.
@@ -34,9 +41,26 @@ public class jframe_sample extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(128, 128, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		txtWelcomeToSupermarket = new JTextField();
+		txtWelcomeToSupermarket.setBounds(30, 24, 396, 25);
+		txtWelcomeToSupermarket.setBackground(new Color(128, 128, 255));
+		txtWelcomeToSupermarket.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		txtWelcomeToSupermarket.setText("Welcome To Supermarket Billing System");
+		contentPane.add(txtWelcomeToSupermarket);
+		txtWelcomeToSupermarket.setColumns(30);
+		
+		JButton btnNewButton = new JButton("Admin login");
+		btnNewButton.setBounds(281, 105, 105, 25);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Cashier Login");
+		btnNewButton_1.setBounds(281, 156, 105, 25);
+		contentPane.add(btnNewButton_1);
 	}
-
 }
---
+++ login_sample.java
@@ -0,0 +1,51 @@
+package swing;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Color;
+
+public class login_sample extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					login_sample frame = new login_sample();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public login_sample() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Login Form");
+		lblNewLabel.setBackground(new Color(255, 128, 64));
+		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 21));
+		lblNewLabel.setBounds(10, 10, 416, 36);
+		contentPane.add(lblNewLabel);
+	}
+}
